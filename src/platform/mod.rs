a162c89378161c9763d888d070c02500cea6c283
HRuin <149753122+HaruNine@users.noreply.github.com>
2026-09-17 18:20:48 +0900

    Merge Linux supplemental hardware sections

--- src/platform/mod.rs
+++ src/platform/mod.rs
@@ -3,6 +3,8 @@ use serde::{Deserialize, Serialize};
 
 #[cfg(target_os = "linux")]
 mod linux;
+#[cfg(target_os = "linux")]
+mod linux_supplement;
 #[cfg(target_os = "macos")]
 mod macos;
 #[cfg(target_os = "windows")]
@@ -27,7 +29,9 @@ pub fn collect() -> Result<Collection, Box<dyn std::error::Error>> {
 
     #[cfg(target_os = "linux")]
     {
-        return linux::collect();
+        let mut collection = linux::collect()?;
+        collection.sections.extend(linux_supplement::collect());
+        return Ok(collection);
     }
 
     #[allow(unreachable_code)]
